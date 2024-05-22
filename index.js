4fc097db4b956caf0ba0d5e871fd9e97f195f0e8
subash gautam <subashgautam977@gmail.com>
2024-05-22 18:45:42 +0545

    Defineing Middleware in separate file

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require("express");
+const reqFilter = require("./middleware");
 const app = express();
 
-const reqFilter = (req, resp, next) => {
-    if (!req.query.age) resp.send("<h1>Provie age plese....");
-    else if (req.query.age < 18)
-        resp.send("<h1>This is age restricted page...");
-    else next();
-};
-// app.use(reqFilter);
+app.use(reqFilter);
 
 app.get("/", (req, resp) => {
     resp.send("<h1> Welcome to home page....");
@@ -15,7 +10,7 @@ app.get("/", (req, resp) => {
 app.get("/about", (req, resp) => {
     resp.send("<h1> I can not say about me rn....");
 });
-app.get("/age", reqFilter /*Single routed middleware*/, (req, resp) => {
+app.get("/contact", (req, resp) => {
     resp.send("<h1> Welcome, You are allowed to visit this page...");
 });
 app.listen(5000, () => {
