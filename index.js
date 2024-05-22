cb682ba5362ad2cd16e182b0f0086565de023290
subash gautam <subashgautam977@gmail.com>
2024-05-22 18:59:58 +0545

    Route level Middleware finished
    Middleware Finished

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const reqFilter = require("./middleware");
 const app = express();
+const route = express.Router();
 
-app.use(reqFilter);
-
+// app.use(reqFilter);
+route.use(reqFilter);
 app.get("/", (req, resp) => {
     resp.send("<h1> Welcome to home page....");
 });
 app.get("/about", (req, resp) => {
     resp.send("<h1> I can not say about me rn....");
 });
-app.get("/contact", (req, resp) => {
+route.get("/contact", (req, resp) => {
     resp.send("<h1> Welcome, You are allowed to visit this page...");
 });
+
+route.get("/main", (req, resp) => {
+    resp.send("<h1> Welcome to main page...");
+});
+
+app.use("/", route);
 app.listen(5000, () => {
     console.log("Port = 5000");
 });
