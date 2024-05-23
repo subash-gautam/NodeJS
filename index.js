5f5dc325050474a4b2683dea5af959ba8fecddfc
subash gautam <subashgautam977@gmail.com>
2024-05-23 17:59:37 +0545

    Reading data from mongoDb

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ const { MongoClient } = require("mongodb");
 const url = "mongodb://localhost:27017";
 const client = new MongoClient(url);
 
-async function getData() {
+async function dbConnet() {
     let result = await client.connect();
     let db = result.db("learn");
-    let collection = db.collection("lrn");
-    let response = await collection.find({}).toArray();
-    console.log(response);
+    return db.collection("lrn");
+    // let response = await collection.find({}).toArray();
+    // console.log(response);
 }
+// // Non recommened way : without making functon
+// dbConnet().then((resp) => {
+//     resp.find()
+//         .toArray()
+//         .then((data) => {
+//             console.log(data);
+//         });
+// });
 
-getData();
-console.log("la hai");
+// recommend way : making functon
+const main = async () => {
+    let data = await dbConnet();
+    data = await data.find().toArray();
+    console.log(data);
+};
+
+main();
