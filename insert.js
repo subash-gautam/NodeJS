1700a29eea85069e57bc75a2b3f53cfe91a02da2
subash gautam <subashgautam977@gmail.com>
2024-05-23 18:31:29 +0545

    Inserting Many data at once

--- insert.js
+++ insert.js
@@ -2,7 +2,12 @@ const dbConnect = require("./mongoDb");
 
 const insert = async () => {
     const db = await dbConnect();
-    const result = await db.insertOne({ name: "Rabin", age: 20 });
+    const result = await db.insertMany([
+        { name: "Rabin", age: 20 },
+        { name: "rawi", age: 25 },
+        { name: "Ravindra", age: 30 },
+        { name: "Rachin", age: 15 },
+    ]);
     if (result.acknowledged) console.log("Data Inserted !!");
     else console.log("Error in inserion. ");
 };
