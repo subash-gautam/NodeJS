f792c5798ad986b77161e08e2a71ae9cd2db3343
Subash Gautam <subashgautam977@gmail.com>
2024-06-03 20:59:06 +0545

    Mongoose put, delete & get

--- index.js
+++ index.js
@@ -14,4 +14,31 @@ app.post("/create", async (req, resp) => {
     resp.send(result);
     // resp.end();
 });
-app.listen(4000);
+
+app.get("/read", async (req, resp) => {
+    let result = await data.find(); //to find all
+    // let result = await data.find({ age: req.body.age }); // Specific
+    console.log(result);
+    resp.send(result);
+});
+
+app.put("/update", async (req, resp) => {
+    if (req.body.ename) {
+        let result = await data.updateOne(
+            { name: req.body.ename },
+            { $set: { name: req.body.name, age: req.body.age } }
+        );
+        resp.send(result);
+    }
+});
+
+app.delete("/delete/:_id", async (req, resp) => {
+    console.log(req.params); // This will log an object like { id: 'some-id' }
+    let result = await data.deleteOne(req.params);
+    resp.send(result);
+    console.log(result);
+});
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+});
