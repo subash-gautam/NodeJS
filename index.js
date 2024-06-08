a5c86f7be1ace71a45db5856555fdad4cf93549a
Subash Gautam <subashgautam977@gmail.com>
2024-06-08 10:26:13 +0545

    SQL put api to update data

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const app = express();
 const port = 3500;
 app.use(express.json());
 
+// read data from sql databse
 app.get("/", (req, resp) => {
     con.query("SELECT * FROM users", (err, result) => {
         if (!err) resp.json(result);
         else console.log(err);
     });
 });
+
+// add data to sql database
 app.post("/", (req, resp) => {
     const data = req.body;
     con.query("INSERT INTO users SET ?", data, (err, result, fields) => {
@@ -18,4 +21,21 @@ app.post("/", (req, resp) => {
     });
 });
 
+// update data of sql database
+app.put("/", (req, resp) => {
+    const data = [req.body.name, req.body.age, req.body.address, req.body.id];
+    con.query(
+        "UPDATE users SET name=?, age =?, address =? WHERE ID=?",
+        data,
+        (err, result, fields) => {
+            if (!err) resp.send(result);
+            else console.log("error : ", err);
+        }
+    );
+});
+
+// delete data from sql database
+app.delete("/", (req, resp) => {
+    const data = req.body;
+});
 app.listen(port, () => console.log(`Listening to port ${port}`));
