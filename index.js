47635f10d0da1574a57b3f112f2b01ee29e1b87e
Subash Gautam <subashgautam977@gmail.com>
2024-06-08 09:37:41 +0545

    SQL post api

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require("express");
 const con = require("./config");
 const app = express();
 const port = 3500;
+app.use(express.json());
 
 app.get("/", (req, resp) => {
     con.query("SELECT * FROM users", (err, result) => {
         if (!err) resp.json(result);
         else console.log(err);
+    });
+});
+app.post("/", (req, resp) => {
+    const data = req.body;
+    con.query("INSERT INTO users SET ?", data, (err, result, fields) => {
+        if (!err) resp.send(result);
+        else console.log("Error encountered: ", err);
     });
 });
 
